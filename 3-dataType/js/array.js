f71cedc2c610b398a1dc1141ea71a71651caac0b
Jonas Oliveira <81438449+jonasnapoles@users.noreply.github.com>
2022-04-12 22:28:53 +0100

    adjust data type

--- 3-dataType/js/array.js
+++ 3-dataType/js/array.js
@@ -1,5 +1,27 @@
 /**
  * Array are considered object in javascript, however they serve as a list;
  * Arrays can have a items of any data type;
- * It is not by key or value, but by index */
+ * It is not by key or value, but by index
+ * 
+ * arry => []
+ * object => {}
+ *  */
 
+var arr = ["United", 10, {country: "Kingdom", worked: true, county: 'Cork'}];
+
+console.log(arr);
+
+var string = ['a', 'b', 'c', 'd']
+
+console.log(string);
+
+var int = [1, 2, 3, 4];
+console.log(int);
+
+console.log(arr[0]);
+
+console.log(int[1]); //output position 1
+
+arr[1] = 5;
+
+console.log(arr);
