01090e7b4358db520700cebd77a4df42ea1d7d0c
Jonas Oliveira <81438449+jonasnapoles@users.noreply.github.com>
2022-04-12 03:41:30 +0100

    update

---
+++ 2-varibles/js/script.js
@@ -0,0 +1,50 @@
+/**
+ * Variables
+ * data Type
+ * @var number[]
+ * @var string[]
+ * @var bbject[]
+ * @var boolean[]
+ * @var null
+ * @var undefiend
+ * Option
+ * @var _Underscore[]
+ * @var $dollarSign[] 
+ */
+var test = 1;
+
+var $variable = [1, 2, 3 , 4, 5];
+
+var _variablewithUnderscore; //camelCase
+var PascalCase = false; //PascalCase
+
+var snake_case_example;
+
+var variable1 = true;
+
+let number = 3;
+
+const $const = true;
+
+console.log(test);
+
+test = "ABC";
+
+console.log(test);
+
+console.log($variable);
+
+console.log(variable1);
+
+console.log(_variablewithUnderscore);
+
+typeof "John"                 // Returns "string"
+typeof 3.14                   // Returns "number"
+typeof NaN                    // Returns "number"
+typeof false                  // Returns "boolean"
+typeof [1,2,3,4]              // Returns "object"
+typeof {name:'John', age:34}  // Returns "object"
+typeof new Date()             // Returns "object"
+typeof function () {}         // Returns "function"
+typeof myCar                  // Returns "undefined" *
+typeof null                   // Returns "object"
