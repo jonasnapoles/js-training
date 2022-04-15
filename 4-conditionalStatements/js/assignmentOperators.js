2bd7dff0320f8873d854c64d6c4be51b0c5f1348
userUbuntu <jno@outlook.pt>
2022-04-15 20:47:22 +0100

    adjust assignment Operators

---
+++ 4-conditionalStatements/js/assignmentOperators.js
@@ -0,0 +1,22 @@
+var age = 15;
+var car;
+if (age <= 17) {
+    console.log("The user can get the driving license");
+    
+}
+
+if (age <=16) {
+    console.log("The user cannot obtain the driving license");
+}
+
+//When we add the variable without attibute It defaults to false. In this way, it ignores this condition.
+if (car) {
+    console.log("He owns a car");
+}
+
+var name = "Julia";
+
+if(name == "Sarah"){
+    console.log("My name is Sarah");
+
+}
